85942b2c8d189931d6c5f083286256878c04daf8
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-03-02 21:47:35 -0600

    Update BendLayer.java

--- src/main/java/com/terraformersmc/terraform/shapes/impl/layer/transform/BendLayer.java
+++ src/main/java/com/terraformersmc/terraform/shapes/impl/layer/transform/BendLayer.java
@@ -35,9 +35,9 @@ public class BendLayer implements Layer {
 	public Position modifyMin(Shape shape) {
 		Position pos = shape.min();
 		double dist = getDist(pos);
-		pos.setX(pos.getX() - dist + height);
-		pos.setY(pos.getY() - dist + height);
-		pos.setZ(pos.getZ() - dist + height);
+		pos.setX(pos.getX() - dist - height);
+		pos.setY(pos.getY() - dist - height);
+		pos.setZ(pos.getZ() - dist - height);
 		return pos;
 	}
 
