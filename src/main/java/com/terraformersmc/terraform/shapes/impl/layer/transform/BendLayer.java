024d695b2fb5a608073dd604640013b69481e2b2
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-03-03 01:57:28 -0600

    Perfected bend layer

--- src/main/java/com/terraformersmc/terraform/shapes/impl/layer/transform/BendLayer.java
+++ src/main/java/com/terraformersmc/terraform/shapes/impl/layer/transform/BendLayer.java
@@ -7,50 +7,55 @@ import com.terraformersmc.terraform.shapes.api.Quaternion;
 import com.terraformersmc.terraform.shapes.api.Shape;
 import com.terraformersmc.terraform.shapes.api.layer.Layer;
 
+import net.minecraft.util.math.MathHelper;
+
 public class BendLayer implements Layer {
 
 	private final double arc;
+	private final double width;
 	private final double height;
 
-	public BendLayer(double arc, double height) {
+	public BendLayer(double arc, double width, double height) {
 		this.arc = arc;
+		this.width = width;
 		this.height = height;
 	}
 
-	public static BendLayer of(double arc, double height) {
-		return new BendLayer(arc, height);
+	public static BendLayer of(double arc, double width, double height) {
+		return new BendLayer(arc, width, height);
 	}
 
 	@Override
 	public Position modifyMax(Shape shape) {
 		Position pos = shape.max();
-		double dist = getDist(pos);
-		pos.setX(pos.getX() + dist + height);
-		pos.setY(pos.getY() + dist + height);
-		pos.setZ(pos.getZ() + dist + height);
+		pos.setY(MathHelper.lerp(reverseLerp(0, 360, arc), pos.getY(), width));
 		return pos;
 	}
 
 	@Override
 	public Position modifyMin(Shape shape) {
 		Position pos = shape.min();
-		double dist = getDist(pos);
-		pos.setX(pos.getX() - dist - height);
-		pos.setY(pos.getY() - dist - height);
-		pos.setZ(pos.getZ() - dist - height);
+		pos.setX(pos.getX() - Math.sin(Math.toRadians(Math.min(arc, 145))) * height);
+		double moss = modifyMax(shape).getY();
+		pos.setY(-MathHelper.lerp(reverseLerp(0, 360, arc), moss, height - moss));
 		return pos;
 	}
 
 	@Override
 	public Predicate<Position> modifyEquation(Shape shape) {
-		return (pos) -> {
-			double dist = getDist(pos);
-			return new RotateLayer(Quaternion.of(0, 0, arc * (dist / height), true)).modifyEquation(shape).test(pos);
-		};
+		return (pos) -> new RotateLayer(getRotation(pos)).modifyEquation(shape).test(pos);
 	}
 
-	private static double getDist(Position pos) {
+	private double getDist(Position pos) {
 		return Math.sqrt(pos.getX() * pos.getX() + pos.getY() + pos.getY() + pos.getZ() + pos.getZ());
+	}
+
+	private Quaternion getRotation(Position pos) {
+		return Quaternion.of(0, 0, arc * (getDist(pos) / height), true);
+	}
+
+	private double reverseLerp(double start, double end, double place) {
+		return ((place - start) / (end - start));
 	}
 
 }
